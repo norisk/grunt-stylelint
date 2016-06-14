796fc5931ae7c1a63767a037403be41b8043f9f7
Amely Kling <akling@noriskshop.de>
2016-06-14 15:57:55 +0200

    Also show details if only warnings occur

--- tasks/grunt-stylelint.js
+++ tasks/grunt-stylelint.js
@@ -29,56 +29,56 @@ module.exports = function ( grunt ) {
 				}
 			}
 
-            grunt.log.writeln("");
+            grunt.log.writeln("\Summary:".bold);
 
-			if ( !data.errored ) {
-				grunt.log.ok( 'Linted ' + options.files.length + ' files without errors' );
-				done();
-			} else {
-				var files_errored = 0,
-                    files_warned = 0,
-					all_warnings = 0,
-					all_errors = 0;
+            var files_errored = 0,
+                files_warned = 0,
+                all_warnings = 0,
+                all_errors = 0;
 
-                for (var i = data.results.length - 1; i >= 0; i--) {
-                    var warnings = 0,
-                        errors = 0;
+            for (var i = data.results.length - 1; i >= 0; i--) {
+                var warnings = 0,
+                    errors = 0;
 
-                    for (var j = data.results[i].warnings.length - 1; j >= 0; j--) {
-                        switch ( data.results[i].warnings[j].severity ) {
-                            case "error":
-                                all_errors++;
-                                errors++;
-                                break;
-                            case "warn":
-                                all_warnings++;
-                                warnings++;
-                                break;
-                        }
+                for (var j = data.results[i].warnings.length - 1; j >= 0; j--) {
+                    switch ( data.results[i].warnings[j].severity ) {
+                        case "error":
+                            all_errors++;
+                            errors++;
+                            break;
+                        case "warn":
+                            all_warnings++;
+                            warnings++;
+                            break;
                     }
-
-                    if (warnings > 0 || errors > 0) {
-
-                        if (errors > 0 ) {
-                            files_errored++;
-                            grunt.log.write(data.results[i].source.red);
-                        } else {
-                            files_warned++;
-                            grunt.log.write(data.results[i].source.yellow);
-                        }
-                        grunt.log.writeln( ( "\t" + errors + " Errors, " + warnings + " Warnings" ).grey );
-                    }
-
-
                 }
-                grunt.log.writeln("");
-                grunt.log.write('Linted ' + options.files.length + " files >> ");
-                grunt.log.write(((options.files.length - files_errored - files_warned) + " files passed ").green);
-                if (all_errors > 0) grunt.log.write((files_errored + " files failed with " + all_errors + " errors ").red);
-                if (all_warnings > 0) grunt.log.write((files_warned + " files warned with " + all_warnings + " warnings ").yellow);
-                grunt.log.writeln("");
-				done( false );
-			}
+
+                if (warnings > 0 || errors > 0) {
+
+                    if (errors > 0 ) {
+                        files_errored++;
+                        grunt.log.write(data.results[i].source.red);
+                    } else {
+                        files_warned++;
+                        grunt.log.write(data.results[i].source.yellow);
+                    }
+                    grunt.log.writeln( ( "\t" + errors + " Errors, " + warnings + " Warnings" ).grey );
+                }
+
+
+            }
+            grunt.log.writeln("");
+            grunt.log.write('Linted ' + options.files.length + " files >> ");
+            grunt.log.write(((options.files.length - files_errored - files_warned) + " files passed").green);
+            if (all_errors > 0) grunt.log.write(", "+(files_errored + " files failed with " + all_errors + " errors").red);
+            if (all_warnings > 0) grunt.log.write(", "+(files_warned + " files warned with " + all_warnings + " warnings").yellow);
+            grunt.log.writeln("");
+            if (data.errored) {
+                done( false );
+            } else {
+                done();
+            }
+
 		}, function ( err ) {
 			grunt.fail.warn( 'Running stylelint failed\n' + err.stack.toString() );
 
